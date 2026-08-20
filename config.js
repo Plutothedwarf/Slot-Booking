c5d3a03c6c1cae4aafba59f5a712d663a788f9e9
Plutothedwarf <147067282+Plutothedwarf@users.noreply.github.com>
2026-08-21 02:16:04 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Paste your deployed Apps Script Web App URL here (see SETUP.md step 4).
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbyRwBbkfmD0pjrvia88Synr2JGkja2Z9IB6f6jlx1U7EpFsOPm1dVlXPermN4XRXQ0b/exec";
 
 async function api(action, payload) {
   const url = API_URL + '?action=' + encodeURIComponent(action) + '&payload=' + encodeURIComponent(JSON.stringify(payload || {}));
